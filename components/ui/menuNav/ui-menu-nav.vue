585a045a6b94f71f11e3a97f947777943252ecc6
Christopher <christophermms.ctt@gmail.com>
2020-11-09 10:05:18 -0300

    Component ui-menu-nav

---
+++ components/ui/menuNav/ui-menu-nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="nav">
+    <NuxtLink v-for="(page, index) in pages" :key="page.name" :to="page.url">
+      {{ page.name }} <i v-if="pages[index+1]" class="fas fa-circle" />
+    </NuxtLink>
+  </nav>
+</template>
+
+<script >
+export default {
+  data () {
+    return {
+      pages: [
+        { name: 'Blog', url: '/' },
+        { name: 'About', url: '/about' },
+        { name: 'Projects', url: '/projects' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped >
+  @import './style.styl';
+</style>
