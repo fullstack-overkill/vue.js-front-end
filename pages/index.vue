8834b0be63577bfafc615cdf70c952e9b951bee5
Christopher <christophermms.ctt@gmail.com>
2020-11-10 11:56:04 -0300

    Page posts

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,28 @@
     <div class="layout-box">
       <h1>Minimalist</h1>
       <h4>powered by christopher</h4>
+      <ui-menu-nav />
+      <hr>
+      <div>
+        <h5>Blog</h5>
+        <div class="grows">
+          <ui-post-resume />
+          <ui-post-resume />
+          <ui-post-resume />
+        </div>
+        <hr>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+
+export default {
+  comments: { }
+}
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
   @import './style.styl';
 </style>
