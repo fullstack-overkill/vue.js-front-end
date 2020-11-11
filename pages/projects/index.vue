04fd761d0b689c27499e96b48c19eee6bc6e4768
Christopher <christophermms.ctt@gmail.com>
2020-11-11 17:02:05 -0300

    📚 General update

---
+++ pages/projects/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page">
+    <div class="layout-box">
+      <h1>Minimalist</h1>
+      <h4>powered by christopher</h4>
+      <ui-menu-nav />
+      <hr>
+      <h5>Projects</h5>
+
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  comments: { }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import './style.styl';
+</style>
